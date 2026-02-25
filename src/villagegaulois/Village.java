d25f8255f0ab9523efaad91c95afa9ddcee9619c
MariaIBSAIENE <nm_ibsaiene@esi.dz>
2026-02-25 11:58:02 +0100

    tp1 version 1

--- src/villagegaulois/Village.java
+++ src/villagegaulois/Village.java
@@ -8,10 +8,12 @@ public class Village {
 	private Chef chef;
 	private Gaulois[] villageois;
 	private int nbVillageois = 0;
+	private Marche marche;
 
-	public Village(String nom, int nbVillageoisMaximum) {
+	public Village(String nom, int nbVillageoisMaximum, int nbEtals) {
 		this.nom = nom;
 		villageois = new Gaulois[nbVillageoisMaximum];
+		 marche= new Marche(nbEtals);
 	}
 
 	public String getNom() {
@@ -20,6 +22,66 @@ public class Village {
 
 	public void setChef(Chef chef) {
 		this.chef = chef;
+	}
+	
+	private class Marche {
+		private Etal[] etals;
+		
+		private Marche(int nbEtals) {
+			etals= new Etal[nbEtals];
+			for(int i=0; i < nbEtals; i++) {
+				etals[i] = new Etal();	}	
+		}
+		
+		private void utiliserEtal(int indiceEtal, Gaulois vendeur,String produit, int nbProduit) {
+			etals[indiceEtal].occuperEtal(vendeur, produit, nbProduit);
+		}
+		
+		private int trouverEtalLibre() {
+			for(int i =0; i< etals.length; i++) {
+				if (! etals[i].isEtalOccupe())
+					return i;
+			}
+			return -1;
+		}
+		
+		private Etal[] trouverEtals (String produit) {
+			int count=0;
+			
+			for(int i=0; i< etals.length; i++) {
+				if(etals[i].contientProduit(produit)) count++;
+			}
+			
+			Etal[] resultat= new Etal[count];
+			int index = 0;
+			
+			for(int i=0; i< etals.length; i++) {
+				if(etals[i].contientProduit(produit)) {
+					resultat[index]= etals[i]; index++; }
+			  }
+			
+			return resultat;
+		}
+		
+		private Etal trouverVendeur(Gaulois gaulois) {
+			for(int i=0; i< etals.length; i++) {
+				if(etals[i].getVendeur() == gaulois) return etals[i];
+			}
+			return null;
+		}
+		
+		private String afficherMarcher() {
+			StringBuilder chaine = new StringBuilder();
+			int count =0;
+			for(int i=0; i< etals.length; i++) {
+				if (etals[i].isEtalOccupe()) {
+				chaine.append(etals[i].afficherEtal());
+				} else count++;
+			}
+			if(count!=0) chaine.append("Il reste "+ count + " étals non utilisés dans le marché." );
+			
+			return chaine.toString();
+		}
 	}
 
 	public void ajouterHabitant(Gaulois gaulois) {
